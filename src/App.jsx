8b17905d65c375cee98ab1494b9d09885d4baa6e
NPK13 <nishalbabupk@gmail.com>
2023-05-23 17:17:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './pages/home'
+import Counter from './pages/counter'
+export default function App() {
+  return (
+    <div>
+        <Home/>
+        <Counter/>
+    </div>
+    
+  )
+}
